305782d312ded3ba741806c2852d2252e060d0e1
AaroLankinen <aaro.lankinen@gmail.com>
2026-04-14 02:33:43 +0300

    Added token usage logging

--- main.py
+++ main.py
@@ -11,4 +11,10 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
-print(response.text)
+if response.usage_metadata is not None:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Total tokens: {response.usage_metadata.total_token_count}")
+    print("Response:", response.text)
+else:
+    raise RuntimeError("Usage metadata not found in response, likely failed API call.")
